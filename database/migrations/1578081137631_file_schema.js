22aab9bf29516d13475533d335fd08d7f2bb8787
Thiago Bruno <tyagobruno@live.com>
2020-05-16 20:58:43 -0300

    Update 1578081137631_file_schema.js

--- database/migrations/1578081137631_file_schema.js
+++ database/migrations/1578081137631_file_schema.js
@@ -10,7 +10,7 @@ class FileSchema extends Schema {
 			table.string('name');
 			table.string('key');
 			table.string('url');
-			table.integer('post_id');
+			table.integer('post_id').unsigned().references('id').inTable('posts').notNullable();
 			table.string('type');
 			table.timestamps();
 		});
