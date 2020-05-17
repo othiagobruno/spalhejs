7d950c01bfe841bd0a282aedb365b47970e33301
Thiago Bruno <tyagobruno@live.com>
2020-05-17 15:51:56 -0300

    sd

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -10,22 +10,24 @@ class FileController {
 				types: [ 'jpeg', 'jpg', 'png' ],
 				size: '15mb'
 			};
-			request.multipart.file('image[]', validationOptions, async (file) => {
-				file.size = file.stream.byteCount;
-				await file.runValidations();
-				const error = file.error();
-				if (error.message) {
-					throw new Error(error.message);
-				}
-				// UPLOAD FILE TO S3
-				const url = await Drive.put(`posts/${params.id + '-' + file.clientName}`, file.stream, {
-					ContentType: file.headers['content-type'],
-					ACL: 'public-read'
-				});
+			for (let i = 0; i < 7; i++) {
+				request.multipart.file(`image[${i}]`, validationOptions, async (file) => {
+					file.size = file.stream.byteCount;
+					await file.runValidations();
+					const error = file.error();
+					if (error.message) {
+						throw new Error(error.message);
+					}
+					// UPLOAD FILE TO S3
+					const url = await Drive.put(`posts/${params.id + '-' + file.clientName}`, file.stream, {
+						ContentType: file.headers['content-type'],
+						ACL: 'public-read'
+					});
 
-				// GARAVA NO BANCO DE DADOS
-				File.create({ url, name: file.clientName, type: file.type, key: params.id });
-			});
+					// GARAVA NO BANCO DE DADOS
+					File.create({ url, name: file.clientName, type: file.type, key: params.id });
+				});
+			}
 			// PROCESSA O ENVIO
 			await request.multipart.process();
 
