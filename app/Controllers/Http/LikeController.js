ce74cf8635edbc8ef0889bec26dc6c33af9d06e7
Thiago Bruno <tyagobruno@live.com>
2020-05-17 09:22:11 -0300

    update likes

--- app/Controllers/Http/LikeController.js
+++ app/Controllers/Http/LikeController.js
@@ -1,35 +1,23 @@
 'use strict';
 
 const Like = use('App/Models/Like');
+
 class LikeController {
 	async store({ auth, request, response }) {
 		const user = auth.current.user;
 		const post_id = request.only([ 'post_id ' ]);
-		try {
-			const like = await Like.query().where('user_id', user.id).where('post_id', post_id).fetch();
-			if (like) {
-				await Like.query().where('user_id', user.id).where('post_id', post_id).delete();
-			} else {
-				await Like.create({ user_id: user.id, post_id });
-			}
-			return response.json({
-				status: 'success',
-				data: like
-			});
-		} catch (error) {
-			return response.json({
-				status: 'error',
-				data: 'error like the post'
-			});
-		}
-	}
 
-	async likePost({ auth, response }) {
 		try {
-			const user = auth.current.user;
-			const post_id = request.only([ 'post_id ' ]);
-			const like = await Like.findOrCreate({ user_id: user.id, post_id }, { user_id: user.id, post_id });
-		} catch (err) {}
+			const like = await Like.findByOrFail({ post_id: post_id, user_id: user.id });
+			like.delete();
+		} catch (err) {
+			await Like.create({ user_id: user.id, post_id });
+		}
+
+		return response.json({
+			status: 'success',
+			data: like
+		});
 	}
 
 	async deslikePost({ params, auth, response }) {
