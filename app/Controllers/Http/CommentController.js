c7a1fc2698b1de218b924b2d63bd8ddc961b19d1
Thiago Bruno <tyagobruno@live.com>
2020-05-17 08:50:19 -0300

    update user comments

--- app/Controllers/Http/CommentController.js
+++ app/Controllers/Http/CommentController.js
@@ -9,7 +9,7 @@ class CommentController {
 
 	async show({ auth, params, response }) {
 		try {
-			const post = await Comment.query().where('postid', params.id).with('user').fetch();
+			const post = await Comment.query().where('post_id', params.id).with('user').fetch();
 			return post;
 		} catch (error) {}
 		return response.send({ error: 'cant find post comments' });
@@ -17,7 +17,7 @@ class CommentController {
 
 	async store({ auth, request, response }) {
 		const user = auth.current.user;
-		const dataReq = request.only([ 'postid', 'text' ]);
+		const dataReq = request.only([ 'post_id', 'text' ]);
 		const data = { ...dataReq, user_id: user.id };
 
 		const comment = await Comment.create(data);
