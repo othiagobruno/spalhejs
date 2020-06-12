4133d679dcf5c3484a0b2af77e77fcf8f010a539
Thiago Bruno <tyagobruno@live.com>
2020-06-12 18:39:12 -0300

    user create update

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -14,7 +14,6 @@ class UserController {
 		try {
 			const me = auth.current.user;
 			//const followed = await me.following().ids();
-
 			const user = await User.query()
 				.where('id', params.id)
 				.withCount('following')
@@ -36,8 +35,13 @@ class UserController {
 
 	// CREATE USER
 	async store({ request, response }) {
-		const data = request.only([ 'name', 'username', 'email', 'password', 'avatar' ]);
-		const user = await User.create(data);
+		const data = request.only([ 'name', 'username', 'email', 'password' ]);
+		const dados = {
+			...data,
+			avatar:
+				'https://firebasestorage.googleapis.com/v0/b/spalhe-app.appspot.com/o/usericon.png?alt=media&token=2c333530-8c82-4d6f-a1ba-dca6410c2036'
+		};
+		const user = await User.create(dados);
 		return response.status(201).send(user);
 	}
 
