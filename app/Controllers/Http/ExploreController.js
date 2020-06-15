f6ee1de5cd512400246a5d037a99ed6704181969
Thiago Bruno <tyagobruno@live.com>
2020-06-14 22:32:48 -0300

    consegui depois de bater cabeçã

--- app/Controllers/Http/ExploreController.js
+++ app/Controllers/Http/ExploreController.js
@@ -5,13 +5,17 @@ const File = use('App/Models/File');
 
 class ExploreController {
 	async index({ auth, response }) {
-		const midias = File.query()
+		const midias = await File.query()
 			.with('posts.user', (builder) => {
-				builder.setVisible([ 'name', 'id', 'username', 'avatar' ]);
+				builder.setVisible([ 'name', 'id', 'username', 'avatar' ]).whereNotNull('id');
 			})
+			.has('posts')
 			.orderBy('id', 'desc')
 			.limit(12)
 			.fetch();
+
+		const data = midias;
+
 		return midias;
 	}
 }
