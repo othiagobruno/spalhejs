0d62810409e6999fd6c6aabc94046a14cd88fc86
Thiago Bruno <tyagobruno@live.com>
2020-06-16 10:57:36 -0300

    update

---
+++ app/Controllers/Http/MomentCommentController.js
@@ -0,0 +1,6 @@
+'use strict'
+
+class MomentCommentController {
+}
+
+module.exports = MomentCommentController
---
+++ database/migrations/1592315699733_moment_comments_schema.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class MomentCommentsSchema extends Schema {
+	up() {
+		this.create('moment_comments', (table) => {
+			table.increments();
+			table.integer('user_id').unsigned().references('id').inTable('users').notNullable().onDelete('CASCADE');
+			table.integer('moment_id').unsigned().references('id').inTable('moments').notNullable().onDelete('CASCADE');
+			table.string('text', 240).notNullable();
+			table.timestamps();
+		});
+	}
+
+	down() {
+		this.drop('moment_comments');
+	}
+}
+
+module.exports = MomentCommentsSchema;
