f4d6b881aebb03a486afa8085272c8179d9a93f8
Thiago Bruno <tyagobruno@live.com>
2020-06-23 20:01:11 -0300

    get user apos update

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,65 +1,89 @@
-'use strict';
+"use strict";
 
-const User = use('App/Models/User');
+const User = use("App/Models/User");
 
 class UserController {
-	// SHOW ALL USERS
-	async index() {
-		const users = await User.all();
-		return users;
-	}
+  // SHOW ALL USERS
+  async index() {
+    const users = await User.all();
+    return users;
+  }
 
-	// SHOW USER BY ID
-	async show({ params, response, auth }) {
-		try {
-			const user = await User.query()
-				.where('id', params.id)
-				.withCount('following')
-				.withCount('followers')
-				.with('followed', (builder) => {
-					builder.where('followid', auth.user.id);
-				})
-				.withCount('posts')
-				.firstOrFail();
+  // SHOW USER BY ID
+  async show({ params, response, auth }) {
+    try {
+      const user = await User.query()
+        .where("id", params.id)
+        .withCount("following")
+        .withCount("followers")
+        .with("followed", (builder) => {
+          builder.where("followid", auth.user.id);
+        })
+        .withCount("posts")
+        .firstOrFail();
 
-			return user;
-		} catch (err) {
-			return response.status(404).json({
-				status: 'error',
-				message: 'user not found'
-			});
-		}
-	}
+      return user;
+    } catch (err) {
+      return response.status(404).json({
+        status: "error",
+        message: "user not found",
+      });
+    }
+  }
 
-	// CREATE USER
-	async store({ request, response, auth }) {
-		const data = request.only([ 'name', 'username', 'email', 'password' ]);
-		const dados = {
-			...data,
-			avatar:
-				'https://firebasestorage.googleapis.com/v0/b/spalhe-app.appspot.com/o/usericon.png?alt=media&token=2c333530-8c82-4d6f-a1ba-dca6410c2036'
-		};
-		const user = await User.findOrCreate({ email: data.email, password: data.password }, dados);
-		if (user) {
-			const { token } = await auth.attempt(data.email, data.password);
-			const user = await User.findBy('email', data.email);
-			return {
-				token,
-				user
-			};
-		} else {
-			return response.status(400).send({ status: 'não foi possivel criar sua conta' });
-		}
-	}
+  // CREATE USER
+  async store({ request, response, auth }) {
+    const data = request.only(["name", "username", "email", "password"]);
+    const dados = {
+      ...data,
+      avatar:
+        "https://firebasestorage.googleapis.com/v0/b/spalhe-app.appspot.com/o/usericon.png?alt=media&token=2c333530-8c82-4d6f-a1ba-dca6410c2036",
+    };
+    const user = await User.findOrCreate(
+      { email: data.email, password: data.password },
+      dados
+    );
+    if (user) {
+      const { token } = await auth.attempt(data.email, data.password);
+      const user = await User.findBy("email", data.email);
+      return {
+        token,
+        user,
+      };
+    } else {
+      return response
+        .status(400)
+        .send({ status: "não foi possivel criar sua conta" });
+    }
+  }
 
-	// UPDATE USER
-	async update({ request, params }) {
-		const { ...data } = request.only([ 'name', 'username', 'email', 'password', 'avatar', 'private', 'biography' ]);
-		const user = await User.findOrFail(params.id);
-		user.merge(data);
-		await user.save();
-		return user;
-	}
+  // UPDATE USER
+  async update({ request, params }) {
+    const { ...data } = request.only([
+      "name",
+      "username",
+      "email",
+      "password",
+      "avatar",
+      "private",
+      "biography",
+    ]);
+    const user = await User.findOrFail(params.id);
+    user.merge(data);
+    const success = await user.save();
+
+    if (success) {
+      const user = await User.query()
+        .where("id", params.id)
+        .withCount("following")
+        .withCount("followers")
+        .withCount("posts")
+        .firstOrFail();
+      return user;
+    }
+
+    return user;
+  }
 }
 
 module.exports = UserController;
