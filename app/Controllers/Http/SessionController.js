fc99acf75cb468306aa21245a1d870b5587d3f97
Thiago Bruno <tyagobruno@live.com>
2020-06-23 20:55:16 -0300

    add user in login

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,17 +1,22 @@
-'use strict';
+"use strict";
 
-const User = use('App/Models/User');
+const User = use("App/Models/User");
 
 class SessionController {
-	async store({ request, auth }) {
-		const { email, password } = request.all();
-		const { token } = await auth.attempt(email, password);
-		const user = await User.findBy('email', email);
-		return {
-			token,
-			user
-		};
-	}
+  async store({ request, auth }) {
+    const { email, password } = request.all();
+    const { token } = await auth.attempt(email, password);
+    const user = await User.query()
+      .where("id", params.id)
+      .withCount("following")
+      .withCount("followers")
+      .withCount("posts")
+      .firstOrFail();
+    return {
+      token,
+      user,
+    };
+  }
 }
 
 module.exports = SessionController;
