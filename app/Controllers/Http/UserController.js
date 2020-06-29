ecdfc5d9ac9c28c7f0026718b0e1c9f3844d743a
Thiago Bruno <tyagobruno@live.com>
2020-06-29 13:41:30 -0300

    user profile

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-const User = use("App/Models/User");
+const User = use('App/Models/User');
 
 class UserController {
   // SHOW ALL USERS
@@ -13,31 +13,30 @@ class UserController {
   async show({ params, response, auth }) {
     try {
       const user = await User.query()
-        .where("id", params.id)
-        .withCount("following")
-        .withCount("followers")
-        .with("followed", (builder) => {
-          builder.where("followid", auth.user.id);
+        .where('id', params.id)
+        .withCount('following')
+        .withCount('followers')
+        .with('followed', (builder) => {
+          builder.where('followid', auth.user.id);
         })
-        .withCount("posts")
+        .withCount('posts')
         .firstOrFail();
 
       return user;
     } catch (err) {
       return response.status(404).json({
-        status: "error",
-        message: "user not found",
+        status: 'error',
+        message: 'user not found',
       });
     }
   }
 
   // CREATE USER
   async store({ request, response, auth }) {
-    const data = request.only(["name", "username", "email", "password"]);
+    const data = request.only(['name', 'username', 'email', 'password']);
     const dados = {
       ...data,
-      avatar:
-        "https://firebasestorage.googleapis.com/v0/b/spalhe-app.appspot.com/o/usericon.png?alt=media&token=2c333530-8c82-4d6f-a1ba-dca6410c2036",
+      avatar: 'https://spalhe-profiles.s3-sa-east-1.amazonaws.com/usericon.png',
     };
     const user = await User.findOrCreate(
       { email: data.email, password: data.password },
@@ -45,7 +44,7 @@ class UserController {
     );
     if (user) {
       const { token } = await auth.attempt(data.email, data.password);
-      const user = await User.findBy("email", data.email);
+      const user = await User.findBy('email', data.email);
       return {
         token,
         user,
@@ -53,7 +52,7 @@ class UserController {
     } else {
       return response
         .status(400)
-        .send({ status: "não foi possivel criar sua conta" });
+        .send({ status: 'não foi possivel criar sua conta' });
     }
   }
 
@@ -61,13 +60,13 @@ class UserController {
   async update({ request, params, auth }) {
     const id = auth.current.user.id;
     const { ...data } = request.only([
-      "name",
-      "username",
-      "email",
-      "password",
-      "avatar",
-      "private",
-      "biography",
+      'name',
+      'username',
+      'email',
+      'password',
+      'avatar',
+      'private',
+      'biography',
     ]);
     const user = await User.findOrFail(id);
     user.merge(data);
@@ -75,10 +74,10 @@ class UserController {
 
     // exibe os dados do usuário
     const userdata = await User.query()
-      .where("id", id)
-      .withCount("following")
-      .withCount("followers")
-      .withCount("posts")
+      .where('id', id)
+      .withCount('following')
+      .withCount('followers')
+      .withCount('posts')
       .firstOrFail();
     return userdata;
   }
