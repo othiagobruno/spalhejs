5a29c795fdd3680f64880ea6bfe8054ae9ab42b3
Thiago Bruno <tyagobruno@live.com>
2020-07-05 10:14:46 -0300

    user required name

--- app/Validators/UserUpdate.js
+++ app/Validators/UserUpdate.js
@@ -6,7 +6,7 @@ class UserUpdate {
   get rules() {
     return {
       name: [
-        rule('required'),
+        // rule('required'),
         rule('string'),
         rule('regex', /^[a-zA-Z ]{3,}$/i),
       ],
@@ -16,6 +16,7 @@ class UserUpdate {
         rule('regex', /^(?=[a-z_\d]*[a-z])[a-z._\d]{5,}$/gim),
       ],
       biography: 'string|max:150',
+      avatar: 'string|max:650',
       website: 'string|url|max:40',
     };
   }
