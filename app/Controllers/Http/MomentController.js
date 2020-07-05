866f95ea603bf47ac117cf62a4343b210730af0f
Thiago Bruno <tyagobruno@live.com>
2020-07-05 14:44:35 -0300

    moments update

--- app/Controllers/Http/MomentController.js
+++ app/Controllers/Http/MomentController.js
@@ -1,23 +1,43 @@
 'use strict';
 
 const Moment = use('App/Models/Moment');
+const User = use('App/Models/User');
 var subDays = require('date-fns/subDays');
 
 class MomentController {
-  async index({ auth }) {
-    const user = auth.current.user;
-    const follows = await user.following().ids();
-    follows.push(user.id);
-    const moments = await Moment.query()
-      .whereIn('user_id', follows)
-      .with('user')
-      .withCount('views')
-      .withCount('comments')
-      .withCount('likes')
-      .where('created_at', '>', subDays(new Date(), 1))
-      .orderBy('id', 'desc')
-      .fetch();
-    return moments;
+  async index({ response, auth }) {
+    try {
+      const user = auth.user;
+      const follows = await user.following().ids();
+      const subDay = subDays(new Date(), 1);
+      follows.push(user.id);
+      const moments = await User.query()
+        .whereIn('id', follows)
+        .select(['id', 'name', 'username', 'avatar'])
+        .whereHas(
+          'moments',
+          (builder) => {
+            builder.where('created_at', '>', subDay);
+          },
+          '>',
+          0
+        )
+        .with('moments', (builder) => {
+          builder
+            .where('created_at', '>', subDay)
+            .withCount('likes')
+            .withCount('comments')
+            .withCount('views');
+        })
+        .orderBy('id', 'desc')
+        .fetch();
+
+      return moments;
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ error: 'error when fetching moments' });
+    }
   }
 
   async store({ auth, request }) {
